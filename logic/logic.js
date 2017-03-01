7b12a55f879f70fe5bf24ed313cca360026ec3c1
NiemandZero <felipegajardo88@gmail.com>
2017-03-01 00:24:05 -0300

    Comments added ~Felipe

--- logic/logic.js
+++ logic/logic.js
@@ -18,17 +18,20 @@ $(document).ready(function () {
                 $("#result").prepend(data[1][0]);
 
                 $("#result").html("");
-
+                
+                //if the API finds data
                 if (data[1].length !== 0) {
 
                     for (var i = 0; i < data[1].length; i++) {
                         $("#result").prepend("<li class='list-group-item text-justify'><a target='blank' href=" + data[3][i] + ">" + data[1][i] + "</a><p class='text-justify'>" + data[2][i] + "</p></li>");
 
                     }
+                //if there is no data from the API
                 } else {
                     $("#result").prepend("<li class='list-group-item text-justify'><a target='blank'>No matches found :( </a><p class='text-justify'></p></li>");
 
                 }
+                //remove the value from the input field
                 $("#searchTerm").val("");
             },
             error: function (errorMessage) {
